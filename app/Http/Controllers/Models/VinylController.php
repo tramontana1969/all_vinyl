415194141d0dcd599843e8b2123a9cea743b237a
tramontana1969 <snegovskojm@gmail.com>
2022-04-08 00:10:18 +0300

    add private convert method

--- app/Http/Controllers/Models/VinylController.php
+++ app/Http/Controllers/Models/VinylController.php
@@ -7,12 +7,21 @@ use App\Models\Vinyl;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
+use phpDocumentor\Reflection\Utils;
 
 class VinylController extends Controller
 {
     public function all() {
         $vinyls = Vinyl::all();
         return view('home', ['vinyls' => $vinyls]);
+    }
+
+    private function convert($img) {
+        $filename = $img->getClientOriginalName();
+        $img->move(Storage::path('public/vinyls/covers/').'origin/', $filename);
+        $thumbnail = Image::make(Storage::path('public/vinyls/covers/').'origin/'.$filename);
+        $thumbnail->fit(300, 300);
+        $thumbnail->save(Storage::path('public/vinyls/covers/').'thumbnail/'.$filename);
     }
 
     public function add(Request $request){
@@ -28,12 +37,8 @@ class VinylController extends Controller
                 ]
             );
         }
-        $filename = $data['cover']->getClientOriginalName();
-        $data['cover']->move(Storage::path('public/vinyls/covers/').'origin/', $filename);
-        $thumbnail = Image::make(Storage::path('public/vinyls/covers/').'origin/'.$filename);
-        $thumbnail->fit(300, 300);
-        $thumbnail->save(Storage::path('public/vinyls/covers/').'thumbnail/'.$filename);
-        $data['cover'] = $filename;
+        $this->convert($data['cover']);
+        $data['cover'] = $data['cover']->getClientOriginalName();;
         $vinyl = new Vinyl($data);
         $vinyl->save();
         return redirect()->refresh();
