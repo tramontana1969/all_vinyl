6929c1efa83b5c3f79d991cac5e34f6434c01214
tramontana1969 <snegovskojm@gmail.com>
2022-05-03 10:16:31 +0300

    change 'Search class to Trait'

--- app/Http/Controllers/Search.php
+++ app/Http/Controllers/Search.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 
-class Search {
-    static function find($query, $columns){
-        return DB::table('vinyls')
-            ->where($columns[0], 'like', "%{$query}%")
-            ->orWhere($columns[1], 'like', "%{$query}%")
-            ->get();
+trait Search {
+    public $table;
+    public $columns;
+    public $query;
+    public function find(){
+        $model = DB::table($this->table)->where($this->columns[0], 'like', "%{$this->query}%");
+        for ($i = 0; $i < count($this->columns); $i++) {
+            $model  ->orWhere($this->columns[$i], 'like', "%{$this->query}%");
+        }
+        return $model->get();
     }
 }
