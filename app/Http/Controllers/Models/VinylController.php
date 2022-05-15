df5e4b8a81bcbca0af4d5fe6af6c638b0ceb9aee
tramontana1969 <snegovskojm@gmail.com>
2022-05-15 16:04:10 +0300

    create VinylDTO

--- app/Http/Controllers/Models/VinylController.php
+++ app/Http/Controllers/Models/VinylController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Models;
 
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Search;
+use App\Http\DataTransferObjects\VinylDTO;
 use App\Models\Vinyl;
 use Illuminate\Http\Request;
 
@@ -12,29 +13,13 @@ class VinylController extends Controller
     use Search;
 
     public function store(Request $request) {
-        Search::$query = $request->input('query');
-        Search::$columns = ['name', 'author'];
-        Search::$table = 'vinyls';
-        $sort = $request->input('sort-by');
-        $vinyls = Search::find();
-        if (count($vinyls) > 0) {
-            switch ($sort){
-                case null:
-                    break;
-                case 'name':
-                    $vinyls=$vinyls->sortBy('name');
-                    break;
-                case 'price':
-                    $vinyls=$vinyls->sortBy('price');
-                    break;
-            }
-            return view('home', ['vinyls' => $vinyls]);
-        }
-        return redirect('/');
+        $vinyls = VinylDTO::vinylRequest($request);
+        return view('home', ['vinyls' => $vinyls]);
     }
 
     public function one($id) {
-        $vinyl = Vinyl::findOrFail($id);
-        return view('one_vinyl', ['vinyl' => $vinyl,]);
+        $model = Vinyl::findOrFail($id);
+        $vinyl = VinylDTO::singleVinyl($model);
+        return view('one_vinyl', ['vinyl' => $vinyl]);
     }
 }
--- app/Http/DataTransferObjects/VinylDTO.php
+++ app/Http/DataTransferObjects/VinylDTO.php
@@ -1,28 +1,49 @@
 <?php
-//
-//namespace App\Http\DataTransferObjects;
-//
-//use App\Http\Controllers\Search;
-//use Illuminate\Http\Request;
-//use Spatie\DataTransferObject\DataTransferObject;
-//
-//final class VinylDTO extends DataTransferObject {
-//
-//    use Search;
-//
-//    public $id;
-//    public $author;
-//    public $name;
-//    public $cover;
-//    public $price;
-//    public $year;
-//    public $description;
-//
-//    public static function VinylRequest(Request $request) {
-//        $query = $request->input('query');
-//        $columns = ['name', 'author'];
-//        $table = 'vinyls';
-//        $sort = $request->input('sort-by');
-//        $vinyls = $this->find();
-//    }
-//}
+
+namespace App\Http\DataTransferObjects;
+
+use App\Http\Controllers\Search;
+use App\Models\Vinyl;
+use Illuminate\Http\Request;
+use Spatie\DataTransferObject\DataTransferObject;
+
+final class VinylDTO extends DataTransferObject {
+
+    use Search;
+
+    public static function vinylRequest(Request $request) {
+        Search::$query = $request->input('query');
+        Search::$columns = ['name', 'author'];
+        Search::$table = 'vinyls';
+        $sort = $request->input('sort-by');
+        $vinyls = Search::find();
+        if (count($vinyls) > 0) {
+            switch ($sort){
+                case null:
+                    break;
+                case 'name':
+                    $vinyls=$vinyls->sortBy('name');
+                    break;
+                case 'price':
+                    $vinyls=$vinyls->sortBy('price');
+                    break;
+            }
+            return $vinyls;
+        }
+        return redirect('/');
+    }
+
+    public static function singleVinyl(Vinyl $vinyl){
+        return [
+            'id' => $vinyl->id,
+            'author' => $vinyl->author,
+            'name' => $vinyl->name,
+            'cover' => $vinyl->cover,
+            'price' => $vinyl->price,
+            'year' => $vinyl->year,
+            'quantity' => $vinyl->quantity,
+            'comment' => $vinyl->comment,
+            'description' => $vinyl->description,
+        ];
+    }
+}
